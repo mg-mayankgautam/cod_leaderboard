e53073c58dcb83870e3f98bae8440b7ba8489297
John Doe <johndoe@email.com>
2024-01-19 18:33:40 +0530

    made pages

---
+++ controller/aboutcontroller.js
@@ -0,0 +1,12 @@
+
+
+module.exports.getabout = async(req,res)=>{
+    //res.send({ "name": "GeeksforGeeks" });
+    res.render('about',{
+        
+  
+     });
+   // res.send('ranking)
+       // console.log('inside get');
+    //res.send('some ranking data');
+}
---
+++ controller/shopcontroller.js
@@ -0,0 +1,12 @@
+
+
+module.exports.getshop = async(req,res)=>{
+    //res.send({ "name": "GeeksforGeeks" });
+    res.render('shop',{
+        
+  
+     });
+   // res.send('ranking)
+       // console.log('inside get');
+    //res.send('some ranking data');
+}
