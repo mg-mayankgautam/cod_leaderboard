53d773ee509905653d4147ed83f563f717f5d6b2
John Doe <johndoe@email.com>
2024-01-31 16:07:12 +0530

    added player dropdown

--- controller/rankingscontroller.js
+++ controller/rankingscontroller.js
@@ -3,21 +3,38 @@ const playerDB = require('../models/player_team_scrimDB')
 const scrimDB = require('../models/scrimDB')
 const teamDB = require('../models/team_scrimDB')
 
-
- module.exports.getRankings = async(req,res)=>{
-//     //res.send({ "name": "GeeksforGeeks" });
+module.exports.getRankings = async(req,res)=>{
     res.render('ranking',{
-        
   
-     });}
+     });
+    }
 
 
- module.exports.getdata= async(req,res)=>{
+module.exports.getscrimdata= async(req,res)=>{
 
-    const allPlayers = await playerDB.find({});
+    // const allPlayers = await playerDB.find({});
     const allScrims=await scrimDB.find({});
-    const allTeams = await teamDB.find({});
+    // const allTeams = await teamDB.find({});
     // console.log("scrims from DB",allScrims)
     
-     res.send({allScrims, allTeams ,allPlayers});
- };    
+     res.send(allScrims);
+};    
+
+
+
+module.exports.getteamsdata= async(req,res)=>{
+    const scrimname = req.query.scrimname;
+    const allTeams = await teamDB.find({scrimname});
+    // console.log("from DB",allScrims)
+    
+     res.send(allTeams);
+};    
+
+
+module.exports.getplayersdata= async(req,res)=>{
+    const scrimname = req.query.scrimname;
+    const allPlayers = await playerDB.find({scrimname});
+      // console.log("from DB",allScrims)
+    
+     res.send(allPlayers);
+};
